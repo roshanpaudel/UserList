c9fcf357b15fcf12bc45494003e51c86a8b85f6a
roshanpaudel <paudelroshan93@gmail.com>
2025-05-10 22:38:34 +1000

    avatar component updated

--- src/components/Avatars.js
+++ src/components/Avatars.js
@@ -1,5 +1,18 @@
 import React from "react";
+import man from "../assets/man.png";
+import woman from "../assets/woman.png";
+export const MaleAvatar = () => {
+  return (
+    <div>
+      <img src={man} alt="man" width="50px" />
+    </div>
+  );
+};
 
-export const Avatars = () => {
-  return <div>Avatars</div>;
+export const FemaleAvatar = () => {
+  return (
+    <div>
+      <img src={woman} alt="woman" width="50px" />
+    </div>
+  );
 };
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FemaleAvatar, MaleAvatar } from "./Avatars";
 
 export const Table = ({ userList }) => {
   return (
@@ -16,9 +17,11 @@ export const Table = ({ userList }) => {
           {userList.map((user, i) => (
             <tr>
               <th scope="row">{i + 1}</th>
-              <td>Mark</td>
-              <td>{user.name}</td>
-              <td>@{user.gender}</td>
+              <td>{user.gender === "m" ? <MaleAvatar /> : <FemaleAvatar />}</td>
+              <td style={{ color: user.gender === "m" ? "blue" : "pink" }}>
+                {user.name}
+              </td>
+              <td>{user.gender}</td>
             </tr>
           ))}
         </tbody>
